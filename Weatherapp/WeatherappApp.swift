471d0ebc1f35c52c4bc0d1fddb484f67fe034224
Darya Mansouri <101394900@clc330-05m.local>
2025-02-27 11:48:36 -0500

    Weather app UI

---
+++ Weatherapp/WeatherappApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherappApp.swift
+//  Weatherapp
+//
+//  Created by Darya Mansouri on 2025-02-27.
+//
+
+import SwiftUI
+
+@main
+struct WeatherappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
